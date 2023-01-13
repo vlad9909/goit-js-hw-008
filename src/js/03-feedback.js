08087f9cdd77aeb1723842d670951443c4aae2c7
vlad9909 <gva9909@gmail.com>
2023-01-13 18:08:53 +0200

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,40 @@
+import throttle from 'lodash.throttle';
 const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('input');
+const textereaRef = document.querySelector('textarea');
+const btnRef = document.querySelector('button');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+saveTxt();
+formRef.addEventListener('input', throttle(inpTxt, 500));
+formRef.addEventListener('submit', onSubmit);
 
-console.log(formRef);
+function inpTxt(evn) {
+  formData[evn.target.name] = evn.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
-const a = document.querySelector('textarea');
+function onSubmit(evn) {
+  evn.preventDefault();
+  if (!inputRef.value || !textereaRef.value) {
+    alert('Всі поля повинні бути заповнені!');
+  }
+  if (inputRef.value && textereaRef.value) {
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    evn.currentTarget.reset();
+  }
+}
 
-console.log(a);
+function saveTxt(evn) {
+  const saveMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedMessage = JSON.parse(saveMessage);
+  if (saveMessage) {
+    if (parsedMessage.email) {
+      inputRef.value = parsedMessage.email;
+    }
+    if (parsedMessage.message) {
+      textereaRef.value = parsedMessage.message;
+    }
+  }
+}
