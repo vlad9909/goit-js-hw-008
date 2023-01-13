7f7aa5266e13b58b817a14cb7f2e9c13a6c8900f
vlad9909 <106588973+vlad9909@users.noreply.github.com>
2023-01-13 16:09:42 +0000

    Deploying to gh-pages from @ vlad9909/goit-js-hw-008@08087f9cdd77aeb1723842d670951443c4aae2c7 🚀

--- 03-feedback.a82761e8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log(e);var o=document.querySelector("textarea");console.log(o)}();
-//# sourceMappingURL=03-feedback.a82761e8.js.map
--- 03-feedback.e6e35acc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.log(e);const o=document.querySelector("textarea");console.log(o);
-//# sourceMappingURL=03-feedback.e6e35acc.js.map
